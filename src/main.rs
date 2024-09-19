a38395953a50f2729abe28ede93c7a915796e086
Michael Taggart <mtaggart@taggart-tech.com>
2024-09-18 23:10:18 -0700

    Token from config

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,10 @@ type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
 /// Status
-#[poise::command(slash_command)]
+#[poise::command(
+    slash_command,
+    required_permissions = "ADMINISTRATOR"
+)]
 async fn status(ctx: Context<'_>) -> Result<(), Error> {
     let response = format!("Everything's a-ok 👍");
     ctx.say(response).await?;
@@ -18,7 +21,10 @@ async fn status(ctx: Context<'_>) -> Result<(), Error> {
 }
 
 /// Add Article
-#[poise::command(slash_command)]
+#[poise::command(
+    slash_command,
+    required_permissions = "MANAGE_MESSAGES"
+)]
 async fn archive(ctx: Context<'_>, 
     #[description = "URL of article"] url: String,
     #[description = "comma-separated list of tags"] tags: Option<String>
@@ -32,7 +38,7 @@ async fn archive(ctx: Context<'_>,
             if !tags.is_empty() {
                 reply.push_str(" with tags ");
                 for t in tags.split(',').collect::<Vec<&str>>() {
-                    reply.push_str(format!("`{t},` ").as_str());
+                    reply.push_str(format!("`{t}` ").as_str());
                 }
             }
             ctx.say(reply).await?;
@@ -55,7 +61,8 @@ pub async fn register(ctx: Context<'_>) -> Result<(), Error> {
 
 #[tokio::main]
 async fn main() {
-    let token = std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN");
+    let config = load_config().unwrap();
+    let token = config.discord_token;
     let intents = serenity::GatewayIntents::non_privileged();
 
     let framework = poise::Framework::builder()
